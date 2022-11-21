9811edab9fc97d529445b7e519c92adc999bf1c3
Agnieszka Bagińska <baginskaagnieszka6@gmail.com>
2022-11-21 12:07:26 +0100

    feat: show an alert with message

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -9,8 +9,16 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup({
-    name: new FormControl (null,[Validators.required]),
+    mail: new FormControl (null,[Validators.required]),
+    // name: new FormControl (null,[Validators.required]),
     salary: new FormControl (null, [Validators.min(0), Validators.required]),
     age: new FormControl (null,[Validators.min(0), Validators.required])
   });
+
+  // onButtonClicked() {
+  //   alert('User was successfully added to the database');
+  // }
+  onButtonClicked(form: {mail:string, age:string, salary:string}) {
+    alert('User was successfully added to the database! ' + 'Name: '+ form.mail + ', Age: '+ form.age + ', Salary: '+form.salary)
+  }
 }
